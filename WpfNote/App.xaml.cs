c2a16a0ada7359d338352cb19ae898f5c739d6d1
Kim <gitmiu@gmail.com>
2021-12-03 15:54:40 +0900

    Initial commit

---
+++ WpfNote/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfNote
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WpfNote/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfNote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
